aab4f3afa4f8546f9f67df163cc9e99e864a843f
monish <monishparthi10@gmail.com>
2024-10-29 17:35:10 +0530

    Global Exceptions and Validations are Handled(implemented)

--- Project-Restful-Webservices/src/main/java/com/Restful_Webservices/Controller/UserController.java
+++ Project-Restful-Webservices/src/main/java/com/Restful_Webservices/Controller/UserController.java
@@ -5,12 +5,17 @@ import com.Restful_Webservices.DTO.UserDto;
 import com.Restful_Webservices.Entity.UserEntity;
 import com.Restful_Webservices.Service.UserService;
 import jakarta.persistence.criteria.CriteriaBuilder;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotEmpty;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+
 
 @RequestMapping("restful-webservice")
 @RestController
@@ -22,7 +27,7 @@ public class UserController
     // REST-FUL API TO SAVE DATAS
     // http://localhost:8080/restful-webservice/save-user
     @PostMapping("save-user")
-    public ResponseEntity<String> saveUser(@RequestBody UserDto dtoSavedUser)
+    public ResponseEntity<String> saveUser(@Valid @RequestBody UserDto dtoSavedUser)
     {
         service.createUser(dtoSavedUser);
         return new ResponseEntity<>("Users are Successfully Added to database", HttpStatus.CREATED);
@@ -31,8 +36,12 @@ public class UserController
     // REST-FUL API TO SAVE LIST OF DATAS
     // http://localhost:8080/restful-webservice/save-all-user
     @PostMapping("save-all-user")
-    public ResponseEntity<String> saveListOfUser(@RequestBody List<UserDto> listOfUsersSaved)
+    public ResponseEntity<String> saveListOfUser( @RequestBody @Valid List<UserDto> listOfUsersSaved, BindingResult bindingResult )
     {
+        if(bindingResult.hasErrors()) {
+            return new ResponseEntity<>("Some datas are missing!!",HttpStatus.BAD_REQUEST);
+        }
+
         service.createAllUser(listOfUsersSaved);
         return new ResponseEntity<>("List of Users are Saved Successfully",HttpStatus.CREATED);
     }
@@ -57,7 +66,7 @@ public class UserController
     // http://localhost:8080/restful-webservice/update-by/7
     @PutMapping("update-by/{id}")
     public ResponseEntity<UserDto> updateRecordsById(@PathVariable Integer id ,
-                                                        @RequestBody UserDto updateRecordsById)
+                                                     @Valid @RequestBody UserDto updateRecordsById)
     {
         updateRecordsById.setId(id);
         return new ResponseEntity<>(service.updateUserById(updateRecordsById),HttpStatus.OK);
--- Project-Restful-Webservices/src/main/java/com/Restful_Webservices/Exception/OwnExceptionHandler.java
+++ Project-Restful-Webservices/src/main/java/com/Restful_Webservices/Exception/OwnExceptionHandler.java
@@ -1,15 +1,24 @@
 package com.Restful_Webservices.Exception;
 
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @ControllerAdvice
-public class OwnExceptionHandler       // IT WILL HANDLE THE BOTH, CUSTOM AS WELL AS GLOBAL EXCEPTION
+public class OwnExceptionHandler extends ResponseEntityExceptionHandler      // IT WILL HANDLE THE BOTH, CUSTOM AS WELL AS GLOBAL EXCEPTION
 {
 
     @ExceptionHandler(value = UserNotFound.class)
@@ -39,5 +48,39 @@ public class OwnExceptionHandler       // IT WILL HANDLE THE BOTH, CUSTOM AS WEL
         );
 
         return new ResponseEntity<>(payloadContainer, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(value = Exception.class)
+    public ResponseEntity<PayloadContainer> handleGlobalException(Exception exception ,
+                                                                             WebRequest webRequest)
+    {
+        PayloadContainer payloadContainer = new PayloadContainer(
+                LocalDateTime.now(),
+                exception.getMessage(),
+                webRequest.getDescription(false),
+                "INTERNAL SERVER ERROR, NO DATAS ARE THERE TO STORE IN DB SERVER"
+        );
+
+        return new ResponseEntity<>(payloadContainer, HttpStatus.BAD_GATEWAY);
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
+                                                                  HttpHeaders headers,
+                                                                  HttpStatusCode status,
+                                                                  WebRequest request)
+    {
+        Map<String,String> validationErrorMessage = new HashMap<String,String>();
+
+        // WE CAN GET ALL THE VALIDATION ERROR MESSAGES FROM MethodArgumentNotValidException OBJECT
+        List<ObjectError> errorList = ex.getBindingResult().getAllErrors();
+        errorList.forEach( (error) ->{
+            // TYPE CASTING INVOLVES HERE
+            String fieldName = ( (FieldError) error).getField();
+            String message = error.getDefaultMessage();
+            validationErrorMessage.put(fieldName,message);
+        });
+        return new ResponseEntity<>(validationErrorMessage, HttpStatus.BAD_REQUEST);
+
     }
 }
